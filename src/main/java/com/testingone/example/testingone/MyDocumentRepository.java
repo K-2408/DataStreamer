0ab231ab93b4253757ec86c8694f77035d52421b
K-2408 <kritithawaria@gmail.com>
2024-05-21 18:09:05 +0530

    Comments rectified

--- src/main/java/com/testingone/example/testingone/MyDocumentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.testingone.example.testingone;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MyDocumentRepository extends MongoRepository<MyDocument, String> {
-}
